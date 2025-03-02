16fab63e12feb9f84f542bbd42eab8a561656f30
Maks DOVHOPOLYI <dovgopolyy0000@gmail.com>
2025-03-02 22:20:39 +0200

    feat: update accounting rules (#1)

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/rollun-com/devops-docker-limits.git /usr/lib/docker-limits
 
+# nvm hack
+ln -fs `which node` /usr/bin/node
+
 npm i --prefix /usr/lib/docker-limits
 
 cp /usr/lib/docker-limits/systemd-service/docker-limits.service /etc/systemd/system/docker-limits.service
